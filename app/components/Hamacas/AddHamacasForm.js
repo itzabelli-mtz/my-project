271076e9f0ab60dbd6b7cc60d61c86f9140c2e6b
itzabelli-mtz <itza252794@gmail.com>
2022-06-22 22:53:50 -0500

    aJam

--- app/components/Hamacas/AddHamacasForm.js
+++ app/components/Hamacas/AddHamacasForm.js
@@ -3,14 +3,33 @@ import React, {useState} from 'react'
 import { Button, Input } from 'react-native-elements'
 import CountryPicker from 'react-native-country-picker-modal'
 import AddHamacas from '../../screens/Hamacas/AddHamacas'
+import { onChange } from 'react-native-reanimated'
 
 export default function AddHamacasForm({toastRef, setLoading, navigation}) {
+    const [formData, setFormData] = useState(defaultFormValues())
+    const [errorName, setErrorName] =useState(null)
+    const [errorDescripcion, setErrorDescription] =useState(null)
+    const [errorEmail, setErrorEmail] =useState(null)
+    const [errorAddress, setErrorAddress] =useState(null)
+    const [errorPhone, setErrorPhone] =useState(null)
+    
+
+
     const addHamacas  = () => {
+        console.log(formData)
         console.log('siii')
     }
   return (
     <View style={styles.viewContainer}>
-      <FormAdd/>
+      <FormAdd
+        formData={formData}
+        setFormData={setFormData}
+        errorName={errorName}
+        errorDescripcion={errorDescripcion}
+        errorEmail={errorEmail}
+        errorAddress={errorAddress}
+        errorPhone={errorPhone}
+      />
       <Button 
         title='Crear negocio'
         onPress={AddHamacas}
@@ -20,22 +39,35 @@ export default function AddHamacasForm({toastRef, setLoading, navigation}) {
   )
 }
 
-function FormAdd() {
+function FormAdd({formData,setFormData, errorName,errorDescripcion,errorEmail,errorAddress,errorPhone}) {
     const [country, setCountry] = useState("MX")
     const [callingCode, setCallingCode] = useState("52")
     const [phone,setPhone] = useState ("")
+
+    const OnChange = (e, type) => {
+        setFormData({...formData, [type] :e.nativeEvent.text})
+    }
 
     return(
         <View styles={styles.viewForm}>
             <Input
                 placeholder='Nombre del negocio'
+                defaultValue={formData.name}
+                onChange={(e) => onChange(e, "name")}
+                errorMessage={errorName}
             />
             <Input
                 placeholder='Dirección del negocio'
+                defaultValue={formData.address}
+                onChange={(e) => onChange(e, "address")}
+                errorMessage={errorAddress}
             />
             <Input
                 keyboardType='email-address'
                 placeholder='Email del negocio'
+                defaultValue={formData.email}
+                onChange={(e) => onChange(e, "email")}
+                errorMessage={errorEmail}
             />
             <View styles={styles.phoneView}>
                 <CountryPicker
@@ -54,15 +86,33 @@ function FormAdd() {
                 placeholder='WhatsApp del negocio'
                 keyboardType='phone-pad'
                 containerStyle={styles.inputPhone}
+                defaultValue={formData.phone}
+                onChange={(e) => onChange(e, "phone")}
+                errorMessage={errorPhone}
                 />
                 <Input
                 placeholder='Descripción del negocio'
                 multiline
                 containerStyle={styles.textArea}
+                defaultValue={formData.description}
+                onChange={(e) => onChange(e, "description")}
+                errorMessage={errorDescripcion}
                 />
             </View>
         </View>
     )
+}
+
+const defaultFormValues = () => {
+    return{
+        name: "",
+        description: "",
+        email:"",
+        phone: "",
+        address: "",
+        country: "MX",
+        callingCode: "52"
+    }
 }
 
 const styles = StyleSheet.create({
